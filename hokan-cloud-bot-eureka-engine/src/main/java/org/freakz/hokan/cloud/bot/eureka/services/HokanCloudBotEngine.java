ebd362fb2f050f4912c8cdc25e17364150a0f82a
Petri Airio <petri.j.airio@gmail.com>
2020-01-12 11:29:24 +0200

    Prepare JPA usage on engine module

--- hokan-cloud-bot-eureka-engine/src/main/java/org/freakz/hokan/cloud/bot/eureka/services/HokanCloudBotEngine.java
+++ hokan-cloud-bot-eureka-engine/src/main/java/org/freakz/hokan/cloud/bot/eureka/services/HokanCloudBotEngine.java
@@ -1,24 +1,26 @@
 package org.freakz.hokan.cloud.bot.eureka.services;
 
 import lombok.extern.slf4j.Slf4j;
-import org.freakz.hokan.cloud.bot.eureka.services.client.IrcIOClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@ComponentScan({"org.freakz.hokan.cloud.bot"})
+@EnableAsync
+//@EnableHystrixDashboard
+//@EnableHystrix
+//@EnableCircuitBreaker
 @EnableFeignClients
+@EnableJpaRepositories("org.freakz.hokan.cloud.bot.common.jpa")
+@EntityScan(basePackages = {"org.freakz.hokan.cloud.bot.common.jpa"})
 @EnableScheduling
 @Slf4j
 @SpringBootApplication
 public class HokanCloudBotEngine {
-
-    @Autowired
-    private IrcIOClient client;
 
     @Value("${spring.application.name}")
     private String appName;
@@ -27,11 +29,4 @@ public class HokanCloudBotEngine {
         SpringApplication.run(HokanCloudBotEngine.class, args);
     }
 
-/*    @Scheduled(fixedRate = 3000)
-    public void timer() {
-        log.debug("Timer!");
-
-        List<IrcServerConfigModel> response = client.serviceRequest();
-        log.debug("response:");
-    }*/
 }
--- hokan-cloud-bot-eureka-io/src/main/java/org/freakz/hokan/cloud/bot/eureka/io/HokanCloudBotEurekaIO.java
+++ hokan-cloud-bot-eureka-io/src/main/java/org/freakz/hokan/cloud/bot/eureka/io/HokanCloudBotEurekaIO.java
@@ -9,12 +9,10 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@ComponentScan({"org.freakz.hokan.cloud.bot"})
 @EnableAsync
 @EnableHystrixDashboard
 @EnableHystrix
